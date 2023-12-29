e87071d48af7fc471ce658fedf9aa1aa408c8501
Pranav Shankar <ramrangparampara@gmail.com>
2023-12-29 11:28:28 +0530

    first commit

---
+++ _config.php
@@ -0,0 +1,29 @@
+<?php 
+
+$websiteTitle = "Cloot"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
+$contactEmail = "pranavshankar101@gmail.com"; // Contact Email
+
+$version = "0.3";
+
+//Donate 
+$donate = "https://cloot.printify.me/products";
+
+// Socials 
+$telegram = "https://t.me/#"; // telegram
+$discord = "https://discord.gg/5HP7pk93"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://indianime.disqus.com"; // Disqus
+
+
+// API URL
+$api = "https://clootapiif--ishanailoveyou.repl.co/"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
